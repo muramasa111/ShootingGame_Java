09a145c5e24b8cbe70d935e0256fa2b4a714224a
masaki murata <wiz.muramasa.111@gmail.com>
2024-06-17 11:46:04 +0900

    練習問題10

--- Shooting/src/PlayerBullet.java
+++ Shooting/src/PlayerBullet.java
@@ -1,5 +1,9 @@
 
 public class PlayerBullet extends Character {
+	public  void draw(MyFrame f) {
+		f.setColor(128,128,128);
+		f.fillRect(x + 10,  y - 30, 10, 20);
+	}
 	public PlayerBullet(double x, double y, double vx, double vy) {
 		super(x, y, vx, vy);
 	}
