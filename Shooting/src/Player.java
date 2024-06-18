4e3caba931c504c5d1a4f488af116950a15c1fce
masaki murata <wiz.muramasa.111@gmail.com>
2024-06-18 10:02:01 +0900

    11.2 敵を動かす

--- Shooting/src/Player.java
+++ Shooting/src/Player.java
@@ -1,13 +1,14 @@
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class Player extends Character implements KeyListener{
-	public  void draw(MyFrame f) {
-		f.setColor(0,128,0);
-		f.fillRect(x,  y + 20, 30, 10);
-		f.setColor(200,200,200);
-		f.fillRect(x + 10,  y, 10, 30);
+public class Player extends Character implements KeyListener {
+	public void draw(MyFrame f) {
+		f.setColor(0, 128, 0);
+		f.fillRect(x, y + 20, 30, 10);
+		f.setColor(200, 200, 200);
+		f.fillRect(x + 10, y, 10, 30);
 	}
+
 	public Player(double x, double y, double vx, double vy) {
 		super(x, y, vx, vy);
 	}
@@ -15,25 +16,22 @@ public class Player extends Character implements KeyListener{
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO 自動生成されたメソッド・スタブ
-		
+
 	}
 
 	@Override
-	public void keyPressed(KeyEvent w	e) {
+	public void keyPressed(KeyEvent e) {
 		// TODO 自動生成されたメソッド・スタブ
-		if(e.getKeyCode() == KeyEvent.VK_LEFT)
-		{
+		if (e.getKeyCode() == KeyEvent.VK_A) {
 			vx = -5;
 		}
-		if(e.getKeyCode() == KeyEvent.VK_RIGHT)
-		{
+		if (e.getKeyCode() == KeyEvent.VK_D) {
 			vx = 5;
 		}
-		if(e.getKeyCode() == KeyEvent.VK_SPACE)
-		{
+		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
 			GameWorld.playerBullets.add(
-					new PlayerBullet(x,y,0,-10));
-			System.out.println("弾の数=" + 
+					new PlayerBullet(x, y, 0, -10));
+			System.out.println("弾の数=" +
 					GameWorld.playerBullets.size());
 		}
 	}
@@ -41,20 +39,20 @@ public class Player extends Character implements KeyListener{
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO 自動生成されたメソッド・スタブ
-		if(e.getKeyCode() == KeyEvent.VK_LEFT)
-		{
+		if (e.getKeyCode() == KeyEvent.VK_A) {
 			vx = 0;
 		}
-		if(e.getKeyCode() == KeyEvent.VK_RIGHT)
-		{
+		if (e.getKeyCode() == KeyEvent.VK_D) {
 			vx = 0;
 		}
 	}
-	
-	public void move(){
+
+	public void move() {
 		super.move();
-		if(x < 0) x = 0;
-		if(x > 370) x = 370;
+		if (x < 0)
+			x = 0;
+		if (x > 370)
+			x = 370;
 	}
-	
+
 }
