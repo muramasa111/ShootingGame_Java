25aa2b7fa3d636202e642fd1d0dbe74f1ebfc304
masaki murata <wiz.muramasa.111@gmail.com>
2024-06-25 09:59:18 +0900

    13.5 クリアしたら次の面に進む

--- Shooting/src/GameFrame.java
+++ Shooting/src/GameFrame.java
@@ -4,7 +4,7 @@ public class GameFrame extends MyFrame {
 	public void run() {
 		GameWorld.player = new Player(100, 300, 0, 0);
 		addKeyListener(GameWorld.player);
-
+		GameWorld.stage = 1;
 		while (true) {
 			GameWorld.player.x = 100;
 			GameWorld.player.y = 300;
@@ -14,6 +14,7 @@ public class GameFrame extends MyFrame {
 			GameWorld.enterPressed = false;
 			while (true) {
 				clear();
+				drawString("Stage = " + GameWorld.stage, 300, 50, 15);
 				GameWorld.player.draw(this);
 				GameWorld.player.move();
 				movePlayerBullets();
@@ -24,13 +25,17 @@ public class GameFrame extends MyFrame {
 				if (GameWorld.enemies.size() == 0) {
 					setColor(0, 0, 0);
 					drawString("クリア！", 100, 200, 40);
-					if (GameWorld.enterPressed)
+					if (GameWorld.enterPressed) {
+						GameWorld.stage++;
 						break;
+					}
 				} else if (GameWorld.player.y < 0) {
 					setColor(0, 0, 0);
 					drawString("ゲームオーバー", 50, 200, 40);
-					if (GameWorld.enterPressed)
+					if (GameWorld.enterPressed) {
+						GameWorld.stage = 1;
 						break;
+					}
 				}
 
 				sleep(0.03);
